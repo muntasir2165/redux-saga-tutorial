54957df72b18dd1c78f7877d9af720b35a92a180
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-20 16:10:04 -0700

    v-15 UsersList ui - part 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUsersRequest } from '../actions/users';
+import UsersList from './UsersList';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.props.getUsersRequest();
   }
 
   render() {
-    return <div>test</div>;
+    const users = this.props.users;
+
+    return (
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <UsersList users={users.items} />
+      </div>
+    );
   }
 }
 
-export default connect(null, { getUsersRequest })(App);
+export default connect(({ users }) => ({ users }), { getUsersRequest })(App);
