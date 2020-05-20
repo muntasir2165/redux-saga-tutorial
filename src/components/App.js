5a0cb8c914a393d957d01f0d86e8621877364a3a
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-20 09:53:21 -0700

    v-9 javascript generator functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
+function* testing() {
+  while (true) {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+}
+
 function App() {
+  const iterator = testing();
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+
   return <div>test</div>;
 }
 
